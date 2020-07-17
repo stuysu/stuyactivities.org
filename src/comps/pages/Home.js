76a33ba9d47f9e2f28949ffcf41d5bca4c8529a7
Abir Taheer <abir@taheer.me>
2020-07-17 15:53:53 -0400

    resized heading and added favicon

--- src/comps/pages/Home.js
+++ src/comps/pages/Home.js
@@ -5,7 +5,7 @@ import { Helmet } from "react-helmet";
 const Home = () => {
 	return (
 		<div>
-			<Typography variant={"h1"}>Welcome home!</Typography>
+			<Typography variant={"h3"}>Welcome home!</Typography>
 			<Helmet>
 				<title>Home | StuyActivities</title>
 			</Helmet>
