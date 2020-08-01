3f5d5e35ba3e1dba45cf2161a062252498570875
ESGit3 <ethanshan706@gmail.com>
2020-08-01 19:33:25 -0400

    added tabs

--- src/comps/pages/organizations/selectedOrganization/CharterTab.js
+++ src/comps/pages/organizations/selectedOrganization/CharterTab.js
@@ -1,16 +1,65 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import { useParams } from "react-router-dom";
-import { Grid, Typography } from "@material-ui/core";
+import {
+	Grid,
+	Typography,
+	Tabs,
+	Tab,
+	Box
+} from "@material-ui/core";
+import {
+	Info,
+	Description,
+	Person
+} from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
 import { gql, useQuery } from "@apollo/client";
 import { client } from "../../../context/ApolloProvider";
 
+//styles
 const useStyles = makeStyles({
 	root: {
-		textAlign: "center",
-		margin: "1em, 0"
+		textAlign: 'center'
+	},
+	name: {
+		marginTop: '1em'
 	}
 });
+
+//tab panels
+function TabPanel(props) {
+	const { children, value, index, ...other } = props;
+
+	return (
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}
+		>
+			{value === index && (
+				<Box p={3}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
+	);
+}
+
+TabPanel.propTypes = {
+	children: PropTypes.node,
+	index: PropTypes.any.isRequired,
+	value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+	return {
+		id: `simple-tab-${index}`,
+		'aria-controls': `simple-tabpanel-${index}`,
+	};
+}
 
 const QUERY = gql`
 	query Organization($url: String) {
@@ -31,17 +80,14 @@ const QUERY = gql`
 	}
 `;
 
-// const { error, data, refetch } = useQuery(QUERY, {
-// 	variables: {
-//
-//
-// 	}
-// });
-// if (error) return <p>There was an error loading this page</p>;
-
 export default function CharterTab(charter) {
 	const url = useParams().url;
 	const classes = useStyles();
+	const [value, setValue] = React.useState(0);
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
 
 	const { data, loading, error } = useQuery(QUERY, {
 		variables: { url },
@@ -60,7 +106,28 @@ export default function CharterTab(charter) {
 		<Grid container spacing={2}>
 			<Grid item xs={1} />
 			<Grid item xs={10} className={classes.root}>
-				<Typography variant={"h3"}>{data.organization.name}</Typography>
+				<Typography variant={"h3"} className={classes.name}>{data.organization.name}</Typography>
+				<Tabs
+					value={value}
+					onChange={handleChange}
+					variant="fullWidth"
+					indicatorColor="primary"
+					textColor="primary"
+					aria-label="icon tabs example"
+				>
+					<Tab icon={<Info />} aria-label="phone" {...a11yProps(0)} />
+					<Tab icon={<Description />} aria-label="favorite" {...a11yProps(1)} />
+					<Tab icon={<Person />} aria-label="person" {...a11yProps(2)} />
+				</Tabs>
+				<TabPanel value={value} index={0}>
+
+				</TabPanel>
+				<TabPanel value={value} index={1}>
+					Item Two
+				</TabPanel>
+				<TabPanel value={value} index={2}>
+					Item Three
+				</TabPanel>
 			</Grid>
 			<Grid item xs={1} />
 		</Grid>
