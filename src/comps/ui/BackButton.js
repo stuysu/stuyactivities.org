5d373292ddc74ce2e8552fd7c0131e16068459e9
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2020-08-09 14:43:59 -0400

    Add magic login page (#22)
    * added route and component for basic token auth
    
    * added catching of expected graphql error so that react doesn't throw it
    
    * fixed colors on charter page
    
    * completed token login front-end
    
    * Restyled by prettier (#23)
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/comps/ui/BackButton.js
+++ src/comps/ui/BackButton.js
@@ -2,11 +2,18 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import UnstyledLink from "./UnstyledLink";
 
-const BackButton = ({ className, to, label }) => {
+const BackButton = ({
+	className,
+	to,
+	label,
+	arrow = true,
+	variant = "outlined",
+	color
+}) => {
 	return (
 		<UnstyledLink to={to}>
-			<Button variant={"outlined"} className={className}>
-				&lt;- {label}
+			<Button variant={variant} className={className} color={color}>
+				{arrow && <span>&lt;-</span>} &nbsp; {label}
 			</Button>
 		</UnstyledLink>
 	);
