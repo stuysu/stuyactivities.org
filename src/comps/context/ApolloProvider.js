178de59c9d9ed77656b64d67755708c7f699f614
Abir Taheer <abir@taheer.me>
2020-08-20 07:02:53 -0400

    exported graphql uri to env variable

--- src/comps/context/ApolloProvider.js
+++ src/comps/context/ApolloProvider.js
@@ -3,11 +3,12 @@ import ApolloClient from "apollo-boost";
 import { InMemoryCache } from "@apollo/client";
 
 import { ApolloProvider as Provider } from "@apollo/react-hooks";
+import { GRAPHQL_URI } from "../../constants";
 
 export const cache = new InMemoryCache();
 
 export const client = new ApolloClient({
-	uri: "https://staging.stuyactivities.org/graphql",
+	uri: GRAPHQL_URI,
 	credentials: "include",
 	cache
 });
