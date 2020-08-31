f2a54d35f77c60504af06c93b6e703d5ed09087f
Victor Veytsman <vityavv@gmail.com>
2020-08-31 15:30:34 -0400

    Change up the membership list looks and make buttons work (#50)
    * Change up the membership list looks and make buttons work
    
    * Change a the mutations that the buttons use
    
    * Fix removal buttons (misspelling in members tab and cache problem)
    
    * fixed approvals
    
    Co-authored-by: Abir Taheer <abir@taheer.me>

--- src/comps/pages/organization/AdminPanelTabs/Requests.js
+++ src/comps/pages/organization/AdminPanelTabs/Requests.js
@@ -1,16 +1,22 @@
 import React from "react";
 import {
-	Button,
 	makeStyles,
-	Paper,
-	Table,
-	TableBody,
-	TableCell,
-	TableContainer,
-	TableHead,
-	TableRow
+	List,
+	ListItem,
+	ListItemAvatar,
+	Avatar,
+	Grid,
+	Typography,
+	ListItemSecondaryAction,
+	IconButton,
+	Dialog,
+	DialogTitle,
+	DialogActions,
+	Button
 } from "@material-ui/core";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery, useMutation } from "@apollo/client";
+import CheckIcon from "@material-ui/icons/Check";
+import CloseIcon from "@material-ui/icons/Close";
 
 const useStyles = makeStyles(theme => ({
 	margin: {
@@ -21,9 +27,11 @@ const useStyles = makeStyles(theme => ({
 const QUERY = gql`
 	query MembershipRequests($url: String!) {
 		membershipRequests(orgUrl: $url) {
+			id
 			user {
 				name
 				email
+				picture
 			}
 			role
 			adminPrivileges
@@ -35,59 +43,161 @@ const QUERY = gql`
 	}
 `;
 
+const APPROVE_MUTATION = gql`
+	mutation ApproveMembershipRequest($requestId: Int!) {
+		approveMembershipRequest(requestId: $requestId) {
+			id
+		}
+	}
+`;
+
+const DELETE_MUTATION = gql`
+	mutation DeleteMembershipRequest($requestId: Int!) {
+		deleteMembershipRequest(requestId: $requestId)
+	}
+`;
+
 export default function Members({ match }) {
 	const classes = useStyles();
 	const { data } = useQuery(QUERY, {
 		variables: { url: match.params.orgUrl }
 	});
+	const [approveMutation] = useMutation(APPROVE_MUTATION);
+	const [deleteMutation] = useMutation(DELETE_MUTATION, {
+		update(cache) {
+			cache.reset();
+		}
+	});
+	const [rejectingRequest, setRejectingRequest] = React.useState({});
+	if (
+		data?.membershipRequests?.filter(
+			request => !request.userApproval || !request.adminApproval
+		)?.length === 0
+	) {
+		return (
+			<Typography variant="h5">
+				No outgoing or incoming requests at this time
+			</Typography>
+		);
+	}
+	const incomingRequests = [],
+		outgoingRequests = [];
+	//would use filter, but forEach prevents going through array twice
+	if (data?.membershipRequests) {
+		data.membershipRequests.forEach(request => {
+			if (!request.userApproval || !request.adminApproval) {
+				request.userApproval
+					? incomingRequests.push(request)
+					: outgoingRequests.push(request);
+			}
+		});
+	}
+	const RequestList = ({ requests }) => {
+		return (
+			<List>
+				{requests?.map(request => (
+					<ListItem>
+						<ListItemAvatar>
+							<Avatar src={request.user.picture} />
+						</ListItemAvatar>
+						<Grid container alignItems={"center"}>
+							<Grid item xl={4} lg={4} md={6} sm={6} xs={12}>
+								<Typography>{request.user.name}</Typography>
+								<Typography
+									color={"textSecondary"}
+									variant={"subtitle2"}
+								>
+									{request.user.email}
+								</Typography>
+							</Grid>
+							<Grid item xl={8} lg={8} md={6} sm={6} xs={12}>
+								<Typography>
+									Desired Role: "{request.role}"{" "}
+									{request.adminPrivileges
+										? "(wants admin)"
+										: ""}
+								</Typography>
+								<Typography>
+									Message: "
+									{request.userApproval
+										? request.userMessage
+										: request.adminMessage}
+									"
+								</Typography>
+							</Grid>
+						</Grid>
+						<ListItemSecondaryAction>
+							{request.userApproval ? (
+								<IconButton
+									onClick={() =>
+										approveMutation({
+											variables: { requestId: request.id }
+										})
+									}
+								>
+									<CheckIcon />
+								</IconButton>
+							) : (
+								""
+							)}
+							<IconButton
+								onClick={() => setRejectingRequest(request)}
+							>
+								<CloseIcon />
+							</IconButton>
+						</ListItemSecondaryAction>
+					</ListItem>
+				))}
+			</List>
+		);
+	};
 	return (
-		<TableContainer component={Paper} className={classes.margin}>
-			<Table>
-				<TableHead>
-					<TableRow>
-						<TableCell>Type</TableCell>
-						<TableCell>Name</TableCell>
-						<TableCell>Email</TableCell>
-						<TableCell>Requested Role</TableCell>
-						<TableCell>Requested Admin?</TableCell>
-						<TableCell>Request Message</TableCell>
-						<TableCell>Actions</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{data?.membershipRequests?.map(request => (
-						<TableRow>
-							<TableCell>
-								{
-									request.userApproval
-										? "Incoming"
-										: "Outgoing"
-									/* assumes if both adminApproval and userApproval are set it is removed. If userApproval is false, then adminApproval must be true */
+		<div className={classes.margin}>
+			{incomingRequests.length > 0 ? (
+				<Typography variant="h5">Incoming Requests</Typography>
+			) : (
+				""
+			)}
+			<RequestList requests={incomingRequests} />
+			{outgoingRequests.length > 0 ? (
+				<Typography variant="h5">Outgoing Requests</Typography>
+			) : (
+				""
+			)}
+			<RequestList requests={outgoingRequests} />
+			<Dialog
+				open={rejectingRequest?.id !== undefined}
+				onClose={() => setRejectingRequest({})}
+			>
+				<DialogTitle>
+					Are you sure you want to{" "}
+					{rejectingRequest?.userApproval
+						? "reject the request from "
+						: "delete the request to"}{" "}
+					{rejectingRequest?.user?.name}?
+				</DialogTitle>
+				<DialogActions>
+					<Button
+						onClick={() => setRejectingRequest({})}
+						color="primary"
+					>
+						Cancel
+					</Button>
+					<Button
+						onClick={() => {
+							deleteMutation({
+								variables: {
+									requestId: rejectingRequest.id
 								}
-							</TableCell>
-							<TableCell>{request.user.name}</TableCell>
-							<TableCell>{request.user.email}</TableCell>
-							<TableCell>{request.role}</TableCell>
-							<TableCell>
-								{request.adminPrivileges ? "Yes" : "No"}
-							</TableCell>
-							<TableCell>
-								{request.userApproval
-									? request.userMessage
-									: request.adminMessage}
-							</TableCell>
-							<TableCell>
-								{request.userApproval ? (
-									<Button color="primary">Approve</Button>
-								) : (
-									""
-								)}
-								<Button color="primary">Remove</Button>
-							</TableCell>
-						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
+							});
+							setRejectingRequest({});
+						}}
+						color="primary"
+					>
+						{rejectingRequest.userApproval ? "Reject" : "Delete"}
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</div>
 	);
 }
