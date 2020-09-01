9c9da4852d018b9a3bdc48ac51891137e65e30ea
Abir Taheer <abir@taheer.me>
2020-08-31 21:05:47 -0400

    added members tab

--- src/comps/pages/organization/MembersTab.js
+++ src/comps/pages/organization/MembersTab.js
@@ -1,7 +1,69 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { OrgContext } from "./OrgRouter";
+import { Typography } from "@material-ui/core";
+import UserContext from "../../context/UserContext";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+
+const getQuery = signedIn => {
+	return gql`
+		query ($orgId: Int!){
+			memberships(orgId: $orgId){
+				role
+				user {
+					name
+					picture
+					${signedIn ? "email" : ""}
+				}
+			}
+		}
+	`;
+};
 
 const MembersTab = () => {
-	return <div>Members</div>;
+	const org = React.useContext(OrgContext);
+	const user = React.useContext(UserContext);
+
+	const { data } = useQuery(getQuery(user.signedIn), {
+		variables: {
+			orgId: org.id
+		}
+	});
+
+	return (
+		<div>
+			<Typography style={{ textAlign: "center" }} variant={"h4"}>
+				Members
+			</Typography>
+
+			{data?.memberships.map(membership => {
+				return (
+					<ListItem key={membership.user.id} button>
+						<ListItemAvatar>
+							<Avatar src={membership.user.picture} />
+						</ListItemAvatar>
+						<span>
+							<Typography>{membership.user.name}</Typography>
+							<Typography
+								color={"textSecondary"}
+								variant={"subtitle2"}
+							>
+								{membership.role}
+							</Typography>
+							<Typography
+								color={"textSecondary"}
+								variant={"subtitle2"}
+							>
+								{membership.user.email}
+							</Typography>
+						</span>
+					</ListItem>
+				);
+			})}
+		</div>
+	);
 };
 
 export default MembersTab;
