54814f042279d2f6a0bdccc51e0d60e91d46bea9
Victor Veytsman <vityavv@gmail.com>
2020-09-17 23:11:55 -0400

    Add existing meetings list and remove functionality

--- src/pages/org/admin/Meetings.js
+++ src/pages/org/admin/Meetings.js
@@ -5,19 +5,33 @@ import {
 	Typography,
 	TextField,
 	Button,
-	InputAdornment
+	InputAdornment,
+	List,
+	ListItem,
+	ListItemText,
+	Paper,
+	IconButton,
+	ListItemSecondaryAction,
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	DialogContentText,
+	DialogActions
 } from "@material-ui/core";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { gql, useMutation } from "@apollo/client";
-import { CalendarToday, Schedule } from "@material-ui/icons";
+import { CalendarToday, Schedule, Edit, Close } from "@material-ui/icons";
+import { OrgContext } from "../index";
+import moment from "moment"
+import ReactMarkdown from "react-markdown";
 
 const useStyles = makeStyles(theme => ({
 	margin: {
 		margin: theme.spacing(1),
 		boxSizing: "border-box"
 	},
-	titleText: {
+	newMeetingTitle: {
 		textAlign: "center",
 		marginBottom: theme.spacing(2)
 	},
@@ -48,12 +62,23 @@ const CREATE_MUTATION = gql`
 			end: $end
 		) {
 			id
+			title
+			description
+			start
+			end
 		}
+	}
+`;
+
+const REMOVE_MUTATION = gql`
+	mutation DeleteMeeting($id: Int!) {
+		deleteMeeting(meetingId: $id)
 	}
 `;
 
 const Meetings = ({ match }) => {
 	const classes = useStyles();
+	const org = React.useContext(OrgContext)
 	const [title, setTitle] = React.useState("");
 	const [description, setDescription] = React.useState("");
 
@@ -67,7 +92,46 @@ const Meetings = ({ match }) => {
 	const [startTime, setStartTime] = React.useState("15:00");
 	const [endTime, setEndTime] = React.useState("17:00");
 
-	const [createMutation] = useMutation(CREATE_MUTATION);
+	const [createMutation] = useMutation(CREATE_MUTATION, {
+		onCompleted() {
+			setTitle("");
+			setDescription("");
+			setDate(
+				`${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
+					2,
+					"0"
+				)}-${today.getDate()}`
+			);
+			setStartTime("15:00");
+			setEndTime("17:00");
+		},
+		update(cache, {data: {createMeeting}}) {
+			cache.modify({
+				id: cache.identify(org),
+				fields: {
+					meetings(existingMeetings = [], {readField}) {
+						const newMeetingRef = cache.writeFragment({
+							data: createMeeting,
+							fragment: gql`
+								fragment NewMeeting on Meeting {
+									id
+									title
+									description
+									start
+									end
+								}
+							`
+						})
+						if (existingMeetings.some(ref => readField("id", ref) === createMeeting.id)) {
+							return existingMeetings
+						} else {
+							return [...existingMeetings, newMeetingRef]
+						}
+					}
+				}
+			})
+		}
+	});
 	const create = () => {
 		createMutation({
 			variables: {
@@ -78,23 +142,30 @@ const Meetings = ({ match }) => {
 				end: new Date(`${date} ${endTime}`)
 			}
 		});
-
-		setTitle("");
-		setDescription("");
-		setDate(
-			`${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
-				2,
-				"0"
-			)}-${today.getDate()}`
-		);
-		setStartTime("15:00");
-		setEndTime("17:00");
 	};
+
+	const [removingMeeting, setRemovingMeeting] = React.useState({})
+	const [removeMutation] = useMutation(REMOVE_MUTATION, {
+		onCompleted() {
+			console.log("COMPLETED RUN")
+			setRemovingMeeting({})
+		},
+		update(cache) {
+			cache.modify({
+				id: cache.identify(org),
+				fields: {
+					meetings(existingMeetings = [], {readField}) {
+						return existingMeetings.filter(ref => removingMeeting.id !== readField("id", ref))
+					}
+				}
+			})
+		}
+	})
 	return (
 		<div className={classes.margin}>
 			<Grid container>
 				<Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-					<Typography variant={"h4"} className={classes.titleText}>
+					<Typography variant={"h4"} className={classes.newMeetingTitle}>
 						New Meeting
 					</Typography>
 					<TextField
@@ -177,11 +248,46 @@ const Meetings = ({ match }) => {
 					</Button>
 				</Grid>
 				<Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-					<Typography variant={"h4"} className={classes.titleText}>
+					<Typography variant={"h4"} style={{textAlign: "center"}}>
 						Existing Meetings
 					</Typography>
+					<List>
+						{org?.meetings?.map(meeting => (
+							<Paper className={classes.margin}>
+								<ListItem>
+									<ListItemText primary={meeting.title} secondary={`${moment(meeting.start).format("dddd, MMMM Do YYYY, h:mm a")} to ${moment(meeting.end).format("h:mm a")}`}/>
+									<ListItemSecondaryAction>
+										<IconButton>
+											<Edit/>
+										</IconButton>
+										<IconButton onClick={() => setRemovingMeeting(meeting)}>
+											<Close/>
+										</IconButton>
+									</ListItemSecondaryAction>
+								</ListItem>
+							</Paper>
+						))}
+					</List>
 				</Grid>
 			</Grid>
+
+			<Dialog open={removingMeeting?.id !== undefined} onClose={() => setRemovingMeeting({})}>
+				<DialogTitle>Are you sure you want to remove {removingMeeting?.title}?</DialogTitle>
+				<DialogContent>
+					<DialogContentText>
+						Description:<br/>
+						<ReactMarkdown source={removingMeeting.description}/>
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={() => setRemovingMeeting({})} color="primary">
+						Cancel
+					</Button>
+					<Button onClick={() => removeMutation({variables: removingMeeting})} color="primary">
+						Remove
+					</Button>
+				</DialogActions>
+			</Dialog>
 		</div>
 	);
 };
