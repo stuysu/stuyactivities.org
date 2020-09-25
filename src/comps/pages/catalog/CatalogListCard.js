cfbf9f3c2e16e7d9bb3ce39e6f701991113efa75
Abir Taheer <abir@taheer.me>
2020-09-25 18:26:16 -0400

    fixed link not working on catalog

--- src/comps/pages/catalog/CatalogListCard.js
+++ src/comps/pages/catalog/CatalogListCard.js
@@ -3,41 +3,45 @@ import { Divider, Grid, ListItem, ListItemAvatar, Typography } from "@material-u
 import Chip from "@material-ui/core/Chip";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import capitalizeString from "../../../utils/capitalizeString";
+import UnstyledLink from "../../ui/UnstyledLink";
 
 export default function CatalogCard({ name, url, tags, charter }) {
 	return (
 		<>
-			<ListItem button style={{ padding: "1rem" }}>
-				<ListItemAvatar>
-					<LazyLoadImage
-						src={charter.picture}
-						width={80}
-						style={{
-							borderRadius: "50%",
-							marginRight: "1rem",
-							width: "80px",
-							height: "80px",
-							objectFit: "cover"
-						}}
-					/>
-				</ListItemAvatar>
-				<div>
-					<Typography>{name}</Typography>
-					<Typography color={"textSecondary"} variant={"subtitle2"}>
-						{charter.mission}
-					</Typography>
-					<Grid container spacing={1}>
-						<Grid item>
-							<Chip label={capitalizeString(charter.commitmentLevel + " Commitment")} />
-						</Grid>
-						{tags.map(tag => (
-							<Grid item key={tag.id}>
-								<Chip key={tag.id} label={tag.name} />
+			<UnstyledLink to={"/" + url}>
+				<ListItem button style={{ padding: "1rem" }}>
+					<ListItemAvatar>
+						<LazyLoadImage
+							src={charter.picture}
+							width={80}
+							style={{
+								borderRadius: "50%",
+								marginRight: "1rem",
+								width: "80px",
+								height: "80px",
+								objectFit: "cover"
+							}}
+						/>
+					</ListItemAvatar>
+					<div>
+						<Typography>{name}</Typography>
+						<Typography color={"textSecondary"} variant={"subtitle2"}>
+							{charter.mission}
+						</Typography>
+						<Grid container spacing={1}>
+							<Grid item>
+								<Chip label={capitalizeString(charter.commitmentLevel + " Commitment")} />
 							</Grid>
-						))}
-					</Grid>
-				</div>
-			</ListItem>
+							{tags.map(tag => (
+								<Grid item key={tag.id}>
+									<Chip key={tag.id} label={tag.name} />
+								</Grid>
+							))}
+						</Grid>
+					</div>
+				</ListItem>
+			</UnstyledLink>
+
 			<Divider />
 		</>
 	);
