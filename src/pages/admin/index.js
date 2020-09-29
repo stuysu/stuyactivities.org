bef3e2aa0243613f86ae5e6fc6e8c92e10ec43e4
Ethan Shan <ethanshan706@gmail.com>
2020-09-28 21:00:39 -0400

    Update index.js

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -5,7 +5,6 @@ import RouteTabs from "../../comps/ui/RouteTabs";
 import Approvals from "./approvals";
 import UserContext from "../../comps/context/UserContext";
 import SignInRequired from "../../comps/ui/SignInRequired";
-import Strikes from "./Strikes";
 import OrgApprovals from "./approvals/OrgApprovals";
 import { AssignmentTurnedIn, LiveHelp } from "@material-ui/icons";
 
@@ -39,11 +38,6 @@ export default function AdminRouter({ match }) {
 			role: "helpRequests",
 			path: actualPath + "/help",
 			icon: <LiveHelp />
-		},
-		{
-			label: "Strikes",
-			role: "strikes",
-			path: actualPath + "/strikes"
 		}
 	].filter(tab => adminRoles.some(row => tab.role === row.role));
 
@@ -60,7 +54,6 @@ export default function AdminRouter({ match }) {
 				<Route path={match.path + "/approvals"} component={Approvals} exact />
 				<Route path={match.path + "/approvals/:url"} component={OrgApprovals} />
 				<Route path={match.path + "/help"} component={Approvals} />
-				<Route path={match.path + "/strikes"} component={Strikes} />
 				<Route path={match.path}>
 					<Redirect to={tabs[0].path} />
 				</Route>
