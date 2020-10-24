daf4620def5dc9f103eb83142e63777b385e28e8
Victor Veytsman <vityavv@gmail.com>
2020-10-23 21:27:03 -0400

    Add help text on login and error page for club admin panel

--- src/pages/org/admin/index.js
+++ src/pages/org/admin/index.js
@@ -11,10 +11,13 @@ import Meetings from "./meetings";
 import { Group, GroupAdd, GroupWork, ListAlt, PostAdd } from "@material-ui/icons";
 import ClubFair from "./club-fair";
 import Updates from "./updates";
+import { OrgContext } from "../index";
+import { Typography } from "@material-ui/core";
 
 export default function OrgAdminRouter({ match }) {
 	const actualMatch = generatePath(match.path, match.params);
 	const user = React.useContext(UserContext);
+	const org = React.useContext(OrgContext);
 
 	if (!user.signedIn) {
 		return <SignInRequired />;
@@ -50,19 +53,25 @@ export default function OrgAdminRouter({ match }) {
 
 	return (
 		<div>
-			<RouteTabs tabs={tabs} />
+			{org?.membership?.adminPrivileges ?
+				<div>
+					<RouteTabs tabs={tabs} />
 
-			<Switch>
-				<Route path={match.path + "/members"} component={Members} />
-				<Route path={match.path + "/member-requests"} component={Requests} />
-				<Route path={match.path + "/charter-edits"} component={CharterEdits} />
-				<Route path={match.path + "/meetings"} component={Meetings} />
-				<Route path={match.path + "/club-fair"} component={ClubFair} />
-				<Route path={match.path + "/posts"} component={Updates} />
-				<Route path={match.path}>
-					<Redirect to={actualMatch + "/members"} />
-				</Route>
-			</Switch>
+					<Switch>
+						<Route path={match.path + "/members"} component={Members} />
+						<Route path={match.path + "/member-requests"} component={Requests} />
+						<Route path={match.path + "/charter-edits"} component={CharterEdits} />
+						<Route path={match.path + "/meetings"} component={Meetings} />
+						<Route path={match.path + "/club-fair"} component={ClubFair} />
+						<Route path={match.path + "/posts"} component={Updates} />
+						<Route path={match.path}>
+							<Redirect to={actualMatch + "/members"} />
+						</Route>
+					</Switch>
+				</div>
+				:
+				<Typography variant={"h2"} align={"center"} style={{maxWidth: "40em", margin: "auto", display: "block"}}>You are not an admin of this club, so you may not access the admin panel!</Typography>
+			}
 		</div>
 	);
 }
