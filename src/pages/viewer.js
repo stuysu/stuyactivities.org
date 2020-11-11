e76d1bba085107c225d78f2b51daf909599863a2
Restyled.io <commits@restyled.io>
2020-11-11 20:42:51 +0000

    Restyled by prettier

--- src/pages/viewer.js
+++ src/pages/viewer.js
@@ -5,51 +5,44 @@ import { gql } from "apollo-boost";
 import ApolloProvider from "../comps/context/ApolloProvider";
 
 const meetingQuery = gql`
-{
-	organizations{
-		  name
-		  meetings {
-			description
-			start
-			end
-		  }
-	  }
-  }
+	{
+		organizations {
+			name
+			meetings {
+				description
+				start
+				end
+			}
+		}
+	}
 `;
 
 const useStyles = makeStyles(theme => ({
 	table: {
 		margin: "auto",
-		padding: "2rem",
+		padding: "2rem"
 	}
 }));
-
 
 function Meeting() {
 	const classes = useStyles();
 
-	const {
-		data
-	} = useQuery(meetingQuery);
+	const { data } = useQuery(meetingQuery);
 
 	const timeOptions = {
 		weekday: "long",
 		year: "numeric",
-		month:"long",
-		day:"numeric",
+		month: "long",
+		day: "numeric",
 		hour: "numeric",
 		minute: "numeric"
-   };
+	};
 
 	if (!data) {
-		return (
-			<div>
-				Loading...
-			</div>
-		);
+		return <div>Loading...</div>;
 	}
 
-	const findZoom = (desc) => {
+	const findZoom = desc => {
 		let re = /https:\/\/(us02web\.)*zoom.us\/j\/.+?\s/;
 		let matches = re.exec(desc);
 		if (matches !== null) {
@@ -62,48 +55,28 @@ function Meeting() {
 		<div>
 			<table border="2" className={classes.table}>
 				<thead>
-					<td>
-						Club Name
-					</td>
-					<td>
-						Start
-					</td>
-					<td>
-						End
-					</td>
-					<td>
-						Description
-					</td>
-					<td>
-						Zoom Link
-					</td>
+					<td>Club Name</td>
+					<td>Start</td>
+					<td>End</td>
+					<td>Description</td>
+					<td>Zoom Link</td>
 				</thead>
 				{data.organizations.map(org => {
 					return org.meetings.map(meeting => {
 						let start_time = new Date(meeting.start);
 						let end_time = new Date(meeting.end);
 
-						if (end_time < (new Date()).getTime()) {
+						if (end_time < new Date().getTime()) {
 							return "";
 						}
 
 						return (
 							<tr>
-								<td>
-									{org.name}
-								</td>
-								<td>
-									{start_time.toLocaleDateString("en-US", timeOptions)}
-								</td>
-								<td>
-									{end_time.toLocaleDateString("en-US", timeOptions)}
-								</td>
-								<td>
-									{meeting.description}
-								</td>
-								<td>
-									{meeting.description !== null ? findZoom(meeting.description) : "" }
-								</td>
+								<td>{org.name}</td>
+								<td>{start_time.toLocaleDateString("en-US", timeOptions)}</td>
+								<td>{end_time.toLocaleDateString("en-US", timeOptions)}</td>
+								<td>{meeting.description}</td>
+								<td>{meeting.description !== null ? findZoom(meeting.description) : ""}</td>
 							</tr>
 						);
 					});
