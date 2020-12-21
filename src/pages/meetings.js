51ccac82f6d26a1ae9393b8e3936255db0ca53cb
Abir Taheer <abir@taheer.me>
2020-12-20 20:46:10 -0500

    fixed calendar styles

--- src/pages/meetings.js
+++ src/pages/meetings.js
@@ -4,7 +4,8 @@ import listPlugin from "@fullcalendar/list";
 import { triggerMeetingDialog } from "../comps/meetings/MeetingPreviewDialog";
 import FullCalendar from "@fullcalendar/react";
 import { gql, useQuery } from "@apollo/client";
-import Loading from "../comps/ui/Loading";
+import { makeStyles, Typography } from "@material-ui/core";
+import FlexCenter from "../comps/ui/FlexCenter";
 
 const now = new Date();
 const firstDay = new Date(now.getFullYear(), now.getMonth(), 1);
@@ -17,35 +18,63 @@ const QUERY = gql`
 			title
 			start
 			end
+			organization {
+				id
+				name
+			}
 		}
 	}
 `;
+
+const useStyles = makeStyles(theme => ({
+	calendarContainer: {
+		width: 1200,
+		maxWidth: "95%"
+	}
+}));
 
 const Meetings = () => {
 	const [start, setStart] = useState(firstDay);
 	const [end, setEnd] = useState(lastDay);
 
-	const { data, loading } = useQuery(QUERY, { variables: { start, end } });
-
-	if (loading) {
-		return <Loading />;
-	}
+	const { data } = useQuery(QUERY, { variables: { start, end } });
+	const classes = useStyles();
 
 	return (
 		<div>
-			<FullCalendar
-				viewDidMount={ev => {
-					setStart(ev.view.activeStart);
-					setEnd(ev.view.activeEnd);
-				}}
-				plugins={[dayGridPlugin, listPlugin]}
-				headerToolbar={{
-					start: "title",
-					end: "dayGridMonth dayGridWeek listYear prev next"
-				}}
-				events={data.meetings}
-				eventClick={ev => triggerMeetingDialog(ev.event.id)}
-			/>
+			<br />
+			<br />
+			<Typography align={"center"} variant={"h2"} paragraph>
+				All Meetings Calendar
+			</Typography>
+			<FlexCenter>
+				<div className={classes.calendarContainer}>
+					<FullCalendar
+						height={800}
+						datesSet={ev => {
+							setStart(ev.view.activeStart);
+							setEnd(ev.view.activeEnd);
+						}}
+						viewDidMount={ev => {
+							setStart(ev.view.activeStart);
+							setEnd(ev.view.activeEnd);
+						}}
+						plugins={[dayGridPlugin, listPlugin]}
+						headerToolbar={{
+							start: "title",
+							end: "dayGridMonth dayGridWeek listYear prev next"
+						}}
+						dayMaxEventRows={4}
+						events={data?.meetings?.map(a => {
+							const newMeeting = { ...a };
+							newMeeting.title = a.organization.name + " - " + a.title;
+							return newMeeting;
+						})}
+						eventClick={ev => triggerMeetingDialog(ev.event.id)}
+					/>
+				</div>
+			</FlexCenter>
+			<br />
 		</div>
 	);
 };
