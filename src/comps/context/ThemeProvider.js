a57b6700ff4539bacc465f4135df6461c139d841
Restyled.io <commits@restyled.io>
2022-08-24 23:42:38 +0000

    Restyled by prettier

--- src/comps/context/ThemeProvider.js
+++ src/comps/context/ThemeProvider.js
@@ -3,59 +3,56 @@ import React from "react";
 import { createTheme, ThemeProvider as Provider } from "@mui/material";
 
 const theme = createTheme({
-		palette: {
-			primary: {
-				main: "#e66767",
-				contrastText: "#fff"
-			},
-			secondary: {
-				// light: "#ff7961",
-				main: "#546de5",
-				// dark: "#ba000d",
-				contrastText: "#fff"
-			}
+	palette: {
+		primary: {
+			main: "#e66767",
+			contrastText: "#fff"
 		},
-		typography: {
-			fontFamily: `'Roboto Condensed', sans-serif`,
-			htmlFontSize: 16,
-			h1: {
-				fontSize: "3rem",
-				fontWeight: 700,
-				margin: "0.5rem 0"
-			},
-			h2: {
-				fontSize: "2.6rem",
-				fontWeight: 700,
-				margin: "0.5rem 0"
-			},
-			h3: {
-				fontSize: "2.2rem",
-				fontWeight: 500,
-				margin: "0.5rem 0"
-			},
-			h4: {
-				fontSize: "1.8rem",
-				fontWeight: 500,
-				margin: "0.5rem 0"
-			},
-			h5: {
-				fontSize: "1.4rem",
-				fontWeight: 400,
-				margin: "0.5rem 0"
-			},
-			h6: {
-				fontSize: "1rem",
-				fontWeight: 400,
-				margin: "0.5rem 0"
-			}
+		secondary: {
+			// light: "#ff7961",
+			main: "#546de5",
+			// dark: "#ba000d",
+			contrastText: "#fff"
+		}
+	},
+	typography: {
+		fontFamily: `'Roboto Condensed', sans-serif`,
+		htmlFontSize: 16,
+		h1: {
+			fontSize: "3rem",
+			fontWeight: 700,
+			margin: "0.5rem 0"
+		},
+		h2: {
+			fontSize: "2.6rem",
+			fontWeight: 700,
+			margin: "0.5rem 0"
+		},
+		h3: {
+			fontSize: "2.2rem",
+			fontWeight: 500,
+			margin: "0.5rem 0"
+		},
+		h4: {
+			fontSize: "1.8rem",
+			fontWeight: 500,
+			margin: "0.5rem 0"
+		},
+		h5: {
+			fontSize: "1.4rem",
+			fontWeight: 400,
+			margin: "0.5rem 0"
+		},
+		h6: {
+			fontSize: "1rem",
+			fontWeight: 400,
+			margin: "0.5rem 0"
 		}
 	}
-);
+});
 
 const ThemeProvider = props => {
-	return (
-		<Provider theme={theme}>{props.children}</Provider>
-	);
+	return <Provider theme={theme}>{props.children}</Provider>;
 };
 
 export default ThemeProvider;
