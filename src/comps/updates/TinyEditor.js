7ee0057c06abd041632354212dcac0f5ba0503a1
David Chen <dchen07@protonmail.com>
2022-10-05 20:58:21 -0400

    Refactor TinyEditor to support dark mode

--- src/comps/updates/TinyEditor.js
+++ src/comps/updates/TinyEditor.js
@@ -1,5 +1,7 @@
+import React from "react";
 import { gql, useMutation } from "@apollo/client";
 import { Editor } from "@tinymce/tinymce-react";
+import { ThemeContext } from "../context/ThemeProvider";
 
 const UPLOAD_MUTATION = gql`
 	mutation ($file: Upload!, $alt: String!) {
@@ -10,60 +12,118 @@ const UPLOAD_MUTATION = gql`
 	}
 `;
 
+const EditorBase = ({
+	value,
+	setValue,
+	uploadPicture,
+	className,
+	disabled,
+	placeholder,
+	skin = undefined,
+	content_css = undefined,
+	content_style = undefined
+}) => (
+	<Editor
+		value={value}
+		apiKey={process.env.NEXT_APP_TINYMCE_APIKEY || "bzg71o9rxjiw3vfmrlmdu07vif9lfs9j50q8h932ajzahz4b"}
+		init={{
+			height: 350,
+			menubar: false,
+			default_link_target: "_blank",
+			plugins: [
+				"advlist autolink lists link image charmap print preview anchor",
+				"searchreplace visualblocks code fullscreen",
+				"insertdatetime media table paste code help wordcount"
+			],
+			toolbar: `formatselect | bold italic forecolor backcolor | 
+			alignleft aligncenter alignright alignjustify | 
+			bullist numlist outdent indent | link image media | removeformat`,
+
+			automatic_uploads: true,
+			browser_spellcheck: true,
+			images_upload_handler: (file, success, failure) => {
+				uploadPicture({
+					variables: {
+						file: file.blob(),
+						alt: "Upload for platform field"
+					}
+				})
+					.then(({ data }) => success(data?.uploadImage?.url))
+					.catch(failure);
+			},
+			images_upload_url: false,
+			media_alt_source: false,
+			media_poster: false,
+			resize_img_proportional: true,
+			media_dimensions: false,
+			placeholder,
+			media_url_resolver: (data, resolve, reject) => {
+				const url = new window.URL(data.url);
+				const allowedHosts = /^(?:\w|\d|\.)*(?:youtube\.com|vimeo\.com|youtu\.be)$/;
+				const hostIsAllowed = !!url.hostname.match(allowedHosts);
+				if (hostIsAllowed) {
+					resolve("");
+				} else {
+					alert("You're only allowed to embed videos from YouTube or Vimeo");
+					reject(new Error("You're only allowed to embed videos from youtube or vimeo"));
+				}
+			},
+			skin,
+			content_css,
+			content_style
+			/*skin: theme.colorMode ? 'oxide-dark' : 'oxide',
+			content_css: theme.colorMode ? 'tinymce-5-dark' : 'default',
+			content_style: theme.colorMode ? 
+				'.mce-content-body[data-mce-placeholder]:not(.mce-visualblocks)::before { color: #888; }': 
+				'',*/
+		}}
+		disabled={disabled}
+		onEditorChange={newValue => setValue(newValue)}
+	/>
+);
+
+const EditorDark = ({ value, setValue, uploadPicture, className, disabled, placeholder }) => (
+	// React won't re-render the Editor component if I only change the props for dark mode, so EditorDark serves as a dummy element
+	<EditorBase
+		value={value}
+		setValue={setValue}
+		className={className}
+		disabled={disabled}
+		placeholder={placeholder}
+		uploadPicture={uploadPicture}
+		skin="oxide-dark"
+		content_css="tinymce-5-dark"
+		content_style=".mce-content-body[data-mce-placeholder]:not(.mce-visualblocks)::before { color: #888; }"
+	/>
+);
+
 const TinyEditor = ({ value, setValue, className, disabled, placeholder }) => {
 	const [uploadPicture] = useMutation(UPLOAD_MUTATION);
-
 	return (
 		<div className={className}>
-			<Editor
-				value={value}
-				apiKey={process.env.NEXT_APP_TINYMCE_APIKEY || "bzg71o9rxjiw3vfmrlmdu07vif9lfs9j50q8h932ajzahz4b"}
-				init={{
-					height: 350,
-					menubar: false,
-					default_link_target: "_blank",
-					plugins: [
-						"advlist autolink lists link image charmap print preview anchor",
-						"searchreplace visualblocks code fullscreen",
-						"insertdatetime media table paste code help wordcount"
-					],
-					toolbar: `formatselect | bold italic forecolor backcolor | 
-					alignleft aligncenter alignright alignjustify | 
-					bullist numlist outdent indent | link image media | removeformat`,
-
-					automatic_uploads: true,
-					browser_spellcheck: true,
-					images_upload_handler: (file, success, failure) => {
-						uploadPicture({
-							variables: {
-								file: file.blob(),
-								alt: "Upload for platform field"
-							}
-						})
-							.then(({ data }) => success(data?.uploadImage?.url))
-							.catch(failure);
-					},
-					images_upload_url: false,
-					media_alt_source: false,
-					media_poster: false,
-					resize_img_proportional: true,
-					media_dimensions: false,
-					placeholder,
-					media_url_resolver: (data, resolve, reject) => {
-						const url = new window.URL(data.url);
-						const allowedHosts = /^(?:\w|\d|\.)*(?:youtube\.com|vimeo\.com|youtu\.be)$/;
-						const hostIsAllowed = !!url.hostname.match(allowedHosts);
-						if (hostIsAllowed) {
-							resolve("");
-						} else {
-							alert("You're only allowed to embed videos from YouTube or Vimeo");
-							reject(new Error("You're only allowed to embed videos from youtube or vimeo"));
-						}
-					}
-				}}
-				disabled={disabled}
-				onEditorChange={newValue => setValue(newValue)}
-			/>
+			<ThemeContext.Consumer>
+				{theme =>
+					theme.colorMode ? (
+						<EditorDark
+							value={value}
+							setValue={setValue}
+							className={className}
+							disabled={disabled}
+							placeholder={placeholder}
+							uploadPicture={uploadPicture}
+						/>
+					) : (
+						<EditorBase
+							value={value}
+							setValue={setValue}
+							className={className}
+							disabled={disabled}
+							placeholder={placeholder}
+							uploadPicture={uploadPicture}
+						/>
+					)
+				}
+			</ThemeContext.Consumer>
 		</div>
 	);
 };
